1f8ea378baa493ea34f5e4fa4c4e886b1736a96c
israncho <isragutierrez@ciencias.unam.mx>
2022-09-30 02:35:36 -0500

    better output

--- practice01/src/DFS.py
+++ practice01/src/DFS.py
@@ -8,7 +8,7 @@ import Node
 from collections import defaultdict
 
 stack = list()
-initial_state = State.state([[1, (), 2],[6, 3, 4],[7, 5, 8]])
+initial_state = State.state([[1, 2, 3],[4, 5, 6],[(), 7, 8]])
 goal_state = State.state([[1, 2, 3],[4, 5, 6],[7, 8, ()]])
 first_node = Node.node(initial_state)
 stack.append(first_node)
@@ -30,11 +30,11 @@ while stack != []:
             new_node.set_previous(move, current_node)
             stack.append(new_node)
 
-print("Nodes visited: ",end="")
+print("\nNodes visited: ",end="")
 print(nodes_visited)
-print("Printing backtrack solution...")
+print("\nPrinting backtrack solution...\n")
 sleep(5)
-solution_moves = 0
+solution_moves = -1
 while current_node != None:
     solution_moves += 1
     print(current_node)
